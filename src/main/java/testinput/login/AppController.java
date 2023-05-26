2fd3b82b72980ff66c87036c277ca67ae18faabc
jcrllano <arellanojuan033@gmail.com>
2023-05-25 22:32:34 -0500

    first Commit

---
+++ src/main/java/testinput/login/AppController.java
@@ -0,0 +1,46 @@
+package testinput.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AppController {
+    Greeting greeting = new Greeting();
+
+
+    @GetMapping("/greeting")
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", greeting);
+        return "greeting";
+    }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+        System.out.println(greeting.getEmail());
+        System.out.println(greeting.getPassword() + " this is the content");
+        String email =  "abbey@yahoo.com";
+        String password = "XDR%6yhn";
+        String UserInputemail =  greeting.getEmail();
+        String UserInputpassword = greeting.getPassword();
+        if (UserInputemail.equals(email)) {
+            if (UserInputpassword.equals(password)) {
+                model.addAttribute("greeting", greeting);  
+                return "result";
+            }
+            return "result2";
+        }
+        else {
+            return "result3";
+        }
+          
+    }
+
+
+    @GetMapping("/signup")
+    public String signup(Model model) {
+        return "signup";
+    }
+}
---
+++ src/main/java/testinput/login/Greeting.java
@@ -0,0 +1,13 @@
+package testinput.login;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class Greeting {
+   private String email;
+   private String password;
+}
