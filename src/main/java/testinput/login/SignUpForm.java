10774ae3a4d575d123f154c9c44d6ba3e6b27ff3
jcrllano <arellanojuan033@gmail.com>
2023-05-31 02:24:49 +0000

    added the SignUpForm java class

---
+++ src/main/java/testinput/login/SignUpForm.java
@@ -0,0 +1,21 @@
+package testinput.login;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity 
+@Table(name="sign_up_form")
+@Getter
+@Setter
+public class SignUpForm {
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipcode;
+}
