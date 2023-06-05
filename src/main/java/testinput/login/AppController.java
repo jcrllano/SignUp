c8a3e51b0e392c3f25286c906109d37d981d024a
jcrllano <arellanojuan033@gmail.com>
2023-06-05 04:42:43 +0000

    modified login page with home index

--- src/main/java/testinput/login/AppController.java
+++ src/main/java/testinput/login/AppController.java
@@ -17,28 +17,6 @@ public class AppController {
         return "greeting";
     }
 
-    @PostMapping("/greeting")
-    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
-        System.out.println(greeting.getEmail());
-        System.out.println(greeting.getPassword() + " this is the content");
-        String email =  "abbey@yahoo.com";
-        String password = "XDR%6yhn";
-        String UserInputemail =  greeting.getEmail();
-        String UserInputpassword = greeting.getPassword();
-
-        //this estatement will check the login input
-        if (UserInputemail.equals(email)) {
-            if (UserInputpassword.equals(password)) {
-                model.addAttribute("greeting", greeting);  
-                return "result";
-            }
-            return "result2";
-        }
-        else {
-            return "result3";
-        }
-          
-    }
 
     //this will invoke the sigup page
     @GetMapping("/signup")
