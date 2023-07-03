620f843adfc1d1c861ca74975445c046da5e2120
jcrllano <arellanojuan033@gmail.com>
2023-07-03 01:08:23 +0000

    added the confirmation class

--- src/main/java/testinput/login/controller/AppController.java
+++ src/main/java/testinput/login/controller/AppController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,6 +17,7 @@ import jakarta.validation.Valid;
 import testinput.login.dto.UserDto;
 import testinput.login.entity.ConfirmationTime;
 import testinput.login.entity.User;
+import testinput.login.service.ConfirmationService;
 import testinput.login.service.TimeService;
 import testinput.login.service.UserService;
 
@@ -25,7 +27,10 @@ public class AppController {
     private UserService userService;
     
     @Autowired 
-    private TimeService timeService;
+    private TimeService timeService; 
+
+    @Autowired
+    private ConfirmationService confirmationService;
 
     public AppController(UserService userService) {
         this.userService = userService;
@@ -41,11 +46,13 @@ public class AppController {
         return "login";
     }
 
-    @GetMapping("/confirmation")
-    public String confirmation(Model model) {
-        
+   /*  @GetMapping("/signup/{day}")
+    String selectTime(Model model, @PathVariable String day) {
+        day = "Monday";
+        var listTime = confirmationService.getAllAvailableTimes();
+        model.addAttribute("listTime", listTime);
         return "confirmation";
-    }
+    }*/
 
     // handler method to handle user registration request
     @GetMapping("/register")
