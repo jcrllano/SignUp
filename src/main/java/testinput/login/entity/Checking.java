216fe5d152db66e2b597a492c9062533817d4974
jcrllano <arellanojuan033@gmail.com>
2023-07-29 04:17:13 +0000

    updated some backgroud code

--- src/main/java/testinput/login/entity/Checking.java
+++ src/main/java/testinput/login/entity/Checking.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,13 +25,13 @@ import lombok.Setter;
 @Table(name="checking")
 public class Checking {
     @Id
-    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;  
 
     private String balance;
-    private String availableBalance;   
-
-    @OneToOne
-    @PrimaryKeyJoinColumn(name = "user_id")
+    private String availableBalance;  
+    
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 }
--- src/main/java/testinput/login/entity/User.java
+++ src/main/java/testinput/login/entity/User.java
@@ -14,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
@@ -57,7 +58,7 @@ public class User {
             inverseJoinColumns={@JoinColumn(name="ROLE_ID", referencedColumnName="ID")})
     private List<Role> roles = new ArrayList<>();
 
-    @OneToOne(mappedBy = "user")
+    @OneToOne(fetch = FetchType.LAZY, cascade =  CascadeType.ALL, mappedBy = "user")
     private Checking checking;
 
 }
